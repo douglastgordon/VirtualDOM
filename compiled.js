a6c96f4471b5c0073163df1d3a6fc0db0a39be59
Douglas Gordon <douglastgordon@gmail.com>
2018-09-24 14:26:46 -0400

    make virtual dom representation

--- compiled.js
+++ compiled.js
@@ -1,1 +1,30 @@
-console.log("hello");
+const view = count => h(
+  "ul",
+  { className: "sample-list" },
+  h(
+    "li",
+    null,
+    "Potatoes"
+  ),
+  h(
+    "li",
+    null,
+    "Carrots"
+  ),
+  h(
+    "li",
+    null,
+    "Oranges"
+  ),
+  h(
+    "li",
+    null,
+    "Lemons"
+  )
+);
+
+const h = (type, props = {}, ...children) => ({
+  type,
+  props,
+  children: children.flat(1)
+});
--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hello")
+const view = count => (
+  <ul className="sample-list">
+    <li>Potatoes</li>
+    <li>Carrots</li>
+    <li>Oranges</li>
+    <li>Lemons</li>
+  </ul>
+)
+
+const h = (type, props={}, ...children) => ({
+  type,
+  props,
+  children: children.flat(1),
+})

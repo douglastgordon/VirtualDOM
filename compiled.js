8cee486588a330cac8d071ac7b3553439966ede1
Douglas Gordon <douglastgordon@gmail.com>
2018-09-24 14:54:29 -0400

    add properties

--- compiled.js
+++ compiled.js
@@ -28,3 +28,31 @@ const h = (type, props = {}, ...children) => ({
   props,
   children: children.flat(1)
 });
+
+const render = node => node.appendChild(createElement(view(0)));
+
+const createElement = node => {
+  const { type, props, children } = node;
+  if (typeof node === "string") return document.createTextNode(node);
+
+  const el = document.createElement(type);
+  props && setProps(el, props);
+  node.children.map(createElement).forEach(el.appendChild.bind(el)); // point free
+  return el;
+};
+
+const setProps = (el, props) => {
+  Object.entries(props).forEach(([name, value]) => {
+    name = name === "className" ? "class" : name;
+    el.setAttribute(name, value);
+  });
+};
+
+// const setProp = (el, name, value) => {
+// }
+
+
+const init = (() => {
+  const root = document.getElementById("root");
+  render(root);
+})();
